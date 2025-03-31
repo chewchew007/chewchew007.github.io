55a8cf304645cc39d315d31eb8aa21a19c376036
chewchew007 <chew.th007@gmail.com>
2025-03-31 13:15:08 +0000

    test clone

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     tailwindcss(),
+    ghPages(), // เพิ่มปลั๊กอินนี้
   ],
-  
+  base: 'chewchew007.github.io', // แทนที่ <REPO_NAME> ด้วยชื่อ repository ของคุณ
 })
